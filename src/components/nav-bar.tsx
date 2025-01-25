077942f1adbdd612a7f986a82c846604fc2c0294
Audrey Lu <audreyylu04@gmail.com>
2025-01-24 22:57:35 -0800

    Added website title

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '@/styles/nav.module.css';
+
+export default function NavBar() {
+  return (
+    <div className={styles["nav-bar"]}>
+      <h1>sprite type</h1>
+    </div>
+  )
+}
